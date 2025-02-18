86f105b089959e9b5e443c4a6aa3ed4a0395e867
PericlesFMartins <periclesrbyamartins@gmail.com>
2025-02-17 21:19:18 -0300

    Front-end-start

---
+++ src/App2.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const App2 = () => {
+  return (
+    <div>App2</div>
+  )
+}
+
+export default App2
+
+//  rafce
+
+// Arrow Function
+// const App2 = () => <h1>Olá, Mundo!</h1>;
+
+// Nomeação de Componente
+// PascalCase
+
+// Nomeação de Variavel, Função...
+// camelCase
+
+// export default, posso importar com qualquer nome e sem chaves
+// export "sem default", só posso importar entre chaves e com o nome exportado
+
+// Self clossing tag
+// <Header></Header>
+// <Header/>
+
+// user agent stylesheet - CSS
+// BoxModel importante aprender em CSS
+
+// Nomeação de classes em CSS
+
+// Metodologia BEM <- buscar mais
+// Blocks
+// Elements
+// Modifiers
+// bloco__elemento--modificador
+// header
+// header__link
+// header__link--small (modificador do elemento link do bloco header)
